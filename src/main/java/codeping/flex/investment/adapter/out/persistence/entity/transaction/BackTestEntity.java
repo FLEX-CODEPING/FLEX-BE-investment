6e9694c8f22c94701c48f3188e0666277a94450c
joowojr <joowojr@gmail.com>
2024-11-18 18:01:02 +0900

    FLEX-239 :sparkles: Feature: 백테스팅 기록을 저장하는 BackTestEntity 구현
    <footer>
    - 관련: #8

---
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/entity/transaction/BackTestEntity.java
@@ -0,0 +1,75 @@
+package codeping.flex.investment.adapter.out.persistence.entity.transaction;
+
+import codeping.flex.investment.adapter.out.persistence.converter.BackTestPeriodConverter;
+import codeping.flex.investment.adapter.out.persistence.entity.common.BaseTimeEntity;
+import codeping.flex.investment.domain.constant.BackTestPeriodType;
+import codeping.flex.investment.adapter.out.persistence.entity.user.UserEntity;
+import codeping.flex.investment.domain.constant.InvestType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "back_test")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BackTestEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private String corpName;
+
+    @Column(nullable = false)
+    private String stockCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column()
+    private InvestType investType;
+
+    @Convert(converter = BackTestPeriodConverter.class)
+    @Column
+    private BackTestPeriodType periodType;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private BigDecimal result;
+
+    @Builder
+    public BackTestEntity(UserEntity userEntity, String stockCode, String corpName, InvestType investType,
+                          BackTestPeriodType backTestPeriodType, LocalDate startDate, LocalDate endDate,
+                          Integer amount, BigDecimal totalPrice, BigDecimal result) {
+        this.user = userEntity;
+        this.stockCode = stockCode;
+        this.corpName = corpName;
+        this.investType = investType;
+        this.periodType = backTestPeriodType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.amount = amount;
+        this.totalPrice = totalPrice;
+        this.result = result;
+    }
+}
