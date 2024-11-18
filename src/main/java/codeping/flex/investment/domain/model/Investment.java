681f0815fa40e7738ae4234ddddba15dee5f6b14
joowojr <joowojr@gmail.com>
2024-11-18 18:13:25 +0900

    FLEX-239 :sparkles: Feature: 변경된 InvestmentEntity의 구조에 따른 수정
    <footer>
    - 관련: #8

--- src/main/java/codeping/flex/investment/domain/model/Investment.java
+++ src/main/java/codeping/flex/investment/domain/model/Investment.java
@@ -1,5 +1,6 @@
 package codeping.flex.investment.domain.model;
 
+import codeping.flex.investment.domain.constant.InvestType;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -7,30 +8,26 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Investment {
-
-    private Long investmentId;
-
+    private Long id;
     private Long userId;
-
-    private String stockName;
-
+    private String corpName;
     private String stockCode;
-
-    private String investType;
-
+    private InvestType investType;
     private int amount;
-
     private BigDecimal price;
-
     private BigDecimal totalPrice;
+    private BigDecimal profit; // 매매 차익
 
     @Builder
-    public Investment(Long userId, String stockName, String stockCode, String investType, int amount, BigDecimal price) {
+    public Investment(Long userId, String stockCode, String corpName, InvestType investType, int amount, BigDecimal price) {
         this.userId = userId;
-        this.stockName = stockName;
         this.stockCode = stockCode;
+        this.corpName = corpName;
         this.investType = investType;
         this.amount = amount;
         this.price = price;
+        this.totalPrice = price.multiply(new BigDecimal(amount));
+        this.profit = BigDecimal.ZERO;
     }
+
 }
