9ddec79ab24db8a1757d13d2d64e1b283d38a3d0
joowojr <joowojr@gmail.com>
2024-11-20 21:32:21 +0900

    FLEX-261 :sparkles: Feat: swagger error 명세 custom 설정
    <footer>
    - 관련: #15

---
+++ src/main/java/codeping/flex/investment/global/config/swagger/ErrorExampleHolder.java
@@ -0,0 +1,13 @@
+package codeping.flex.investment.global.config.swagger;
+
+import io.swagger.v3.oas.models.examples.Example;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorExampleHolder {
+	private Example example;
+	private String name;
+	private int code;
+}
