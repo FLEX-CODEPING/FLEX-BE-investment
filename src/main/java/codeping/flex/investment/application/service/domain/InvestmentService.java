4a7efeb24dfcb7cfb5a686b3169b4424c0f31a07
jiwon <kiwijomn@gmail.com>
2024-11-22 17:28:33 +0900

    FLEX-242 ✨ Feat: 매수 Investment 저장 usecase 추가 및 구현
    <footer>
    - 관련: #10

---
+++ src/main/java/codeping/flex/investment/application/service/domain/InvestmentService.java
@@ -0,0 +1,24 @@
+package codeping.flex.investment.application.service.domain;
+
+import codeping.flex.investment.adapter.in.web.data.investment.request.BuyStockRequest;
+import codeping.flex.investment.application.ports.in.investment.InvestmentUseCase;
+import codeping.flex.investment.application.ports.out.InvestmentOutPort;
+import codeping.flex.investment.domain.constant.InvestType;
+import codeping.flex.investment.domain.model.stockportfolio.Investment;
+import codeping.flex.investment.global.annotation.architecture.ApplicationService;
+import lombok.RequiredArgsConstructor;
+
+import static codeping.flex.investment.application.mapper.InvestmentMapper.mapToInvestment;
+
+@ApplicationService
+@RequiredArgsConstructor
+public class InvestmentService implements InvestmentUseCase {
+
+    private final InvestmentOutPort investmentOutPort;
+
+    @Override
+    public Investment saveBuyTypeInvestment(Long userId, BuyStockRequest buyStockRequest) {
+        Investment investment = mapToInvestment(userId, InvestType.BUY, buyStockRequest);
+        return investmentOutPort.saveInvestment(investment);
+    }
+}
