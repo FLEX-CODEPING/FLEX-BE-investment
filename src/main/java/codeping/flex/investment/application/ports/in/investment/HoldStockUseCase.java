9d6c192d72f297a56334b17c828ade4a73952322
jiwon <kiwijomn@gmail.com>
2024-11-22 21:16:17 +0900

    FLEX-242 ✨ Feat: HoldStock 관련 usecase 추가 및 구현
    <footer>
    - 관련: #10

---
+++ src/main/java/codeping/flex/investment/application/ports/in/investment/HoldStockUseCase.java
@@ -0,0 +1,12 @@
+package codeping.flex.investment.application.ports.in.investment;
+
+import codeping.flex.investment.domain.model.stockportfolio.HoldStock;
+import codeping.flex.investment.domain.model.stockportfolio.Investment;
+
+import java.util.Optional;
+
+public interface HoldStockUseCase {
+
+    Optional<HoldStock> getHoldStockByUserIdAndStockCode(Long userId, String stockCode);
+    HoldStock saveHoldStock(Long userId, String stockCode, String corpName, long totalHoldings, Investment recentInvestment);
+}
---
+++ src/main/java/codeping/flex/investment/application/service/domain/HoldStockService.java
@@ -0,0 +1,30 @@
+package codeping.flex.investment.application.service.domain;
+
+import codeping.flex.investment.application.ports.in.investment.HoldStockUseCase;
+import codeping.flex.investment.application.ports.out.HoldStockOutPort;
+import codeping.flex.investment.domain.model.stockportfolio.HoldStock;
+import codeping.flex.investment.domain.model.stockportfolio.Investment;
+import codeping.flex.investment.global.annotation.architecture.ApplicationService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import static codeping.flex.investment.application.mapper.HoldStockMapper.mapToHoldStock;
+
+@ApplicationService
+@RequiredArgsConstructor
+public class HoldStockService implements HoldStockUseCase {
+
+    private final HoldStockOutPort holdStockOutPort;
+
+    @Override
+    public Optional<HoldStock> getHoldStockByUserIdAndStockCode(Long userId, String stockCode) {
+        return holdStockOutPort.getHoldStockByUserIdAndStockCode(userId, stockCode);
+    }
+
+    @Override
+    public HoldStock saveHoldStock(Long userId, String stockCode, String corpName, long totalHoldings, Investment recentInvestment) {
+        HoldStock holdStock = mapToHoldStock(userId, stockCode, corpName, totalHoldings, recentInvestment);
+        return holdStockOutPort.saveHoldStock(holdStock);
+    }
+}
