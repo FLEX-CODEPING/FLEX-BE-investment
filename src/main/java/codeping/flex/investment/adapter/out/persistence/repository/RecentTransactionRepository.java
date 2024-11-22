e914b0ef403b7814eaef34ea9456b623058786c2
jiwon <kiwijomn@gmail.com>
2024-11-22 20:48:01 +0900

    FLEX-242 ✨ Feat: RecentTransaction repository 레이어 추가
    <footer>
    - 관련: #10

---
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/repository/RecentTransactionRepository.java
@@ -0,0 +1,13 @@
+package codeping.flex.investment.adapter.out.persistence.repository;
+
+import codeping.flex.investment.adapter.out.persistence.entity.stockportfolio.RecentTransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RecentTransactionRepository extends JpaRepository<RecentTransactionEntity, Long> {
+
+    Optional<RecentTransactionEntity> findByUserId(Long userId);
+}
