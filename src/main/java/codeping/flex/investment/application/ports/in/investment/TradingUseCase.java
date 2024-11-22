a444a1bf52668c2d9b83d467e4ea56f2867fe4ce
jiwon <kiwijomn@gmail.com>
2024-11-22 22:33:13 +0900

    FLEX-242 ✨ Feat: 매수 API Response 추가
    <footer>
    - 관련: #10

--- src/main/java/codeping/flex/investment/application/ports/in/investment/TradingUseCase.java
+++ src/main/java/codeping/flex/investment/application/ports/in/investment/TradingUseCase.java
@@ -1,8 +1,9 @@
 package codeping.flex.investment.application.ports.in.investment;
 
 import codeping.flex.investment.adapter.in.web.data.investment.request.BuyStockRequest;
+import codeping.flex.investment.adapter.in.web.data.investment.response.BuyStockResponse;
 
 public interface TradingUseCase {
 
-    void buyStocks(Long userId, BuyStockRequest buyStockRequest);
+    BuyStockResponse buyStocks(Long userId, BuyStockRequest buyStockRequest);
 }
