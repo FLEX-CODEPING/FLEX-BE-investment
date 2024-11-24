312c4b696261edb080672922f47758254c0c3ca0
jiwon <kiwijomn@gmail.com>
2024-11-24 15:38:39 +0900

    FLEX-243 ♻️ Refactor: 매매 Investment 저장 method 구현 리팩토링
    <footer>
    - 관련: #11

--- src/main/java/codeping/flex/investment/application/service/domain/InvestmentService.java
+++ src/main/java/codeping/flex/investment/application/service/domain/InvestmentService.java
@@ -1,6 +1,7 @@
 package codeping.flex.investment.application.service.domain;
 
 import codeping.flex.investment.adapter.in.web.data.trading.request.BuyStockRequest;
+import codeping.flex.investment.adapter.in.web.data.trading.request.SellStockRequest;
 import codeping.flex.investment.application.ports.in.investment.domain.InvestmentUseCase;
 import codeping.flex.investment.application.ports.out.InvestmentOutPort;
 import codeping.flex.investment.domain.constant.InvestType;
@@ -20,7 +21,19 @@ public class InvestmentService implements InvestmentUseCase {
 
     @Override
     public Investment saveBuyTypeInvestment(Long userId, BuyStockRequest buyStockRequest, BigDecimal totalPrice) {
-        Investment investment = mapToInvestment(userId, InvestType.BUY, buyStockRequest, totalPrice);
+        Investment investment = mapToInvestment(
+                userId, buyStockRequest.stockCode(), buyStockRequest.corpName(), InvestType.BUY,
+                buyStockRequest.quantity(), buyStockRequest.price(), totalPrice, BigDecimal.ZERO
+        );
+        return investmentOutPort.saveInvestment(investment);
+    }
+
+    @Override
+    public Investment saveSellTypeInvestment(Long userId, SellStockRequest sellStockRequest, BigDecimal totalPrice, BigDecimal profit) {
+        Investment investment = mapToInvestment(
+                userId, sellStockRequest.stockCode(), sellStockRequest.corpName(), InvestType.SELL,
+                sellStockRequest.quantity(), sellStockRequest.price(), totalPrice, profit
+        );
         return investmentOutPort.saveInvestment(investment);
     }
 }
