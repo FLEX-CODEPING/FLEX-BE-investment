925856111af4e3be8175aaf017fc7fdcdfbf0c9e
jiwon <kiwijomn@gmail.com>
2024-11-24 23:57:42 +0900

    FLEX-242 ✨ Feat: Swagger Error Code 명세 추가
    <footer>
    - 관련: #10

--- src/main/java/codeping/flex/investment/adapter/in/web/api/TradingController.java
+++ src/main/java/codeping/flex/investment/adapter/in/web/api/TradingController.java
@@ -6,13 +6,19 @@ import codeping.flex.investment.application.ports.in.investment.TradingUseCase;
 import codeping.flex.investment.global.annotation.architecture.WebAdapter;
 import codeping.flex.investment.global.annotation.passport.Passport;
 import codeping.flex.investment.global.annotation.passport.PassportInfo;
+import codeping.flex.investment.global.annotation.swagger.ApiErrorCodes;
 import codeping.flex.investment.global.common.response.ApplicationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static codeping.flex.investment.domain.exception.InvestmentErrorCode.BALANCE_NOT_SUFFICIENT;
+import static codeping.flex.investment.domain.exception.RecentTransactionErrorCode.RECENT_TRANSACTION_NOT_FOUND;
 
 @WebAdapter
 @RestController
@@ -24,9 +30,13 @@ public class TradingController {
 
     @PostMapping("/buy")
     @Operation(summary = "매수", description = "특정 종목에 대한 주식을 매수합니다.")
+    @ApiErrorCodes(
+            investmentErrors = {BALANCE_NOT_SUFFICIENT},
+            recentTransactionErrors = {RECENT_TRANSACTION_NOT_FOUND}
+    )
     public ApplicationResponse<BuyStockResponse> buyStocks(
             @Parameter(hidden = true) @Passport PassportInfo passportInfo,
-            BuyStockRequest buyStockRequest
+            @RequestBody @Valid BuyStockRequest buyStockRequest
     ) {
         BuyStockResponse buyStockResponse = tradingUseCase.buyStocks(passportInfo.userId(), buyStockRequest);
         return ApplicationResponse.onSuccess(buyStockResponse);
