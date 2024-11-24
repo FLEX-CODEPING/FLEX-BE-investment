2b9b7fcbf6ad2a71d27837737a7a68ccdf50a044
jiwon <kiwijomn@gmail.com>
2024-11-24 15:37:47 +0900

    FLEX-243 ✨ Feat: 매도 Investment 저장 method 추가
    <footer>
    - 관련: #11

--- src/main/java/codeping/flex/investment/application/ports/in/investment/domain/InvestmentUseCase.java
+++ src/main/java/codeping/flex/investment/application/ports/in/investment/domain/InvestmentUseCase.java
@@ -1,6 +1,7 @@
 package codeping.flex.investment.application.ports.in.investment.domain;
 
 import codeping.flex.investment.adapter.in.web.data.trading.request.BuyStockRequest;
+import codeping.flex.investment.adapter.in.web.data.trading.request.SellStockRequest;
 import codeping.flex.investment.domain.model.Investment;
 
 import java.math.BigDecimal;
@@ -10,4 +11,6 @@ public interface InvestmentUseCase {
     /** SAVE **/
     // 매수 정보 저장
     Investment saveBuyTypeInvestment(Long userId, BuyStockRequest buyStockRequest, BigDecimal totalPrice);
+    // 매도 정보 저장
+    Investment saveSellTypeInvestment(Long userId, SellStockRequest sellStockRequest, BigDecimal totalPrice, BigDecimal profit);
 }
