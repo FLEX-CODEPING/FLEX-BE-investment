88f97c00fb87958bd08b96dd395b5de5037f0b80
jiwon <kiwijomn@gmail.com>
2024-11-24 15:49:52 +0900

    FLEX-243 ✨ Feat: 매도 Response Mapper 추가
    <footer>
    - 관련: #11

--- src/main/java/codeping/flex/investment/application/mapper/TradingResponseMapper.java
+++ src/main/java/codeping/flex/investment/application/mapper/TradingResponseMapper.java
@@ -1,6 +1,7 @@
 package codeping.flex.investment.application.mapper;
 
 import codeping.flex.investment.adapter.in.web.data.trading.response.BuyStockResponse;
+import codeping.flex.investment.adapter.in.web.data.trading.response.SellStockResponse;
 import codeping.flex.investment.domain.model.Investment;
 
 import java.math.BigDecimal;
@@ -18,6 +19,19 @@ public class TradingResponseMapper {
                 investment.getQuantity(),
                 investment.getPrice(),
                 investment.getTotalPrice(),
+                currentBalance,
+                investment.getCreatedAt()
+        );
+    }
+
+    public static SellStockResponse mapToSellStockResponse(Investment investment, BigDecimal currentBalance) {
+        return new SellStockResponse(
+                investment.getStockCode(),
+                investment.getCorpName(),
+                investment.getQuantity(),
+                investment.getPrice(),
+                investment.getTotalPrice(),
+                investment.getProfit(),
                 currentBalance,
                 investment.getCreatedAt()
         );
