b78587aa769ea4633136777d462f0f662049673f
jiwon <kiwijomn@gmail.com>
2024-11-27 22:20:11 +0900

    FLEX-292 ♻️ Refactor: CustomSliceResponse 리팩토링
    <footer>
    - 관련: #20

--- src/main/java/codeping/flex/investment/adapter/in/web/data/pagination/CustomSliceResponse.java
+++ src/main/java/codeping/flex/investment/adapter/in/web/data/pagination/CustomSliceResponse.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Slice;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -28,12 +29,12 @@ public class CustomSliceResponse<T> implements Serializable {
         this.isLast = isLast;
     }
 
-    public static <T> CustomSliceResponse<T> of(List<T> contents, boolean hasNext, boolean isFirst, boolean isLast) {
+    public static <T, R> CustomSliceResponse<T> of(List<T> contents, Slice<R> sliceResult) {
         return CustomSliceResponse.<T>builder()
                 .content(contents)
-                .hasNext(hasNext)
-                .isFirst(isFirst)
-                .isLast(isLast)
+                .hasNext(sliceResult.hasNext())
+                .isFirst(sliceResult.isFirst())
+                .isLast(sliceResult.isLast())
                 .build();
     }
 }
