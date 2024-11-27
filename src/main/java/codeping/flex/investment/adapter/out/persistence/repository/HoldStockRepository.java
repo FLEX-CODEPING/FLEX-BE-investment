5e5dab1b4565089790fa503ca12a574889fd41a6
jiwon <kiwijomn@gmail.com>
2024-11-27 22:35:02 +0900

    FLEX-292 ✨ Feat: HoldStock Query Method 추가
    <footer>
    - 관련: #20

--- src/main/java/codeping/flex/investment/adapter/out/persistence/repository/HoldStockRepository.java
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/repository/HoldStockRepository.java
@@ -1,6 +1,9 @@
 package codeping.flex.investment.adapter.out.persistence.repository;
 
 import codeping.flex.investment.adapter.out.persistence.entity.HoldStockEntity;
+import codeping.flex.investment.domain.constant.HoldStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,5 @@ import java.util.Optional;
 public interface HoldStockRepository extends JpaRepository<HoldStockEntity, Long> {
 
     Optional<HoldStockEntity> findByUserIdAndStockCode(Long userId, String stockCode);
+    Slice<HoldStockEntity> findAllByUserIdAndHoldStatus(Long userId, HoldStatus holdStatus, Pageable pageable);
 }
