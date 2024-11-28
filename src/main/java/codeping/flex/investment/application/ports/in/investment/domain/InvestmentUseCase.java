e0978ae5cc0307b616c5eef9ba8558afb2f88d25
jiwon <kiwijomn@gmail.com>
2024-11-28 11:25:27 +0900

    FLEX-294 ✨ Feat: 특정 유저의 특정 종목 매매 내역 조회 usecase 구현
    <footer>
    - 관련: #24

--- src/main/java/codeping/flex/investment/application/ports/in/investment/domain/InvestmentUseCase.java
+++ src/main/java/codeping/flex/investment/application/ports/in/investment/domain/InvestmentUseCase.java
@@ -1,5 +1,8 @@
 package codeping.flex.investment.application.ports.in.investment.domain;
 
+import codeping.flex.investment.adapter.in.web.data.investment.request.UserStockInvestmentRequest;
+import codeping.flex.investment.adapter.in.web.data.investment.response.UserStockInvestmentResponse;
+import codeping.flex.investment.adapter.in.web.data.pagination.CustomSliceResponse;
 import codeping.flex.investment.adapter.in.web.data.trading.request.BuyStockRequest;
 import codeping.flex.investment.domain.model.Investment;
 
@@ -10,4 +13,8 @@ public interface InvestmentUseCase {
     /** SAVE **/
     // 매수 정보 저장
     Investment saveBuyTypeInvestment(Long userId, BuyStockRequest buyStockRequest, BigDecimal totalPrice);
+
+    /** GET **/
+    // 특정 유저의 특정 종목 매매 내역 조회
+    CustomSliceResponse<UserStockInvestmentResponse> getAllUserStockInvestments(Long userId, UserStockInvestmentRequest userStockInvestmentRequest);
 }
