c4a196bc72d3866d3bcd905bfba8b23deb68cb43
jiwon <kiwijomn@gmail.com>
2024-11-28 10:25:41 +0900

    FLEX-293 ✨ Feat: Transaction Query Method 추가
    <footer>
    - 관련: #23

--- src/main/java/codeping/flex/investment/adapter/out/persistence/repository/TransactionRepository.java
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/repository/TransactionRepository.java
@@ -1,9 +1,13 @@
 package codeping.flex.investment.adapter.out.persistence.repository;
 
 import codeping.flex.investment.adapter.out.persistence.entity.TransactionEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
+
+    Slice<TransactionEntity> findAllByUserId(Long userId, Pageable pageable);
 }
