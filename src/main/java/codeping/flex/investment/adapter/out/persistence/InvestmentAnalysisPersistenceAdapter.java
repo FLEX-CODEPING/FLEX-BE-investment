82b93924feafbd249b3248a10d91389e4b21a427
LimpidusCoruscare <chelhwankim@gmail.com>
2024-12-08 15:22:19 +0900

    FLEX-325 ✨ Feat: 투자 분석 기능 구현

---
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/InvestmentAnalysisPersistenceAdapter.java
@@ -0,0 +1,42 @@
+package codeping.flex.investment.adapter.out.persistence;
+
+import codeping.flex.investment.adapter.out.persistence.entity.InvestmentEntity;
+import codeping.flex.investment.adapter.out.persistence.mapper.InvestmentPersistenceMapper;
+import codeping.flex.investment.adapter.out.persistence.repository.InvestmentRepository;
+import codeping.flex.investment.application.ports.out.InvestmentAnalysisOutPort;
+import codeping.flex.investment.domain.model.Investment;
+import codeping.flex.investment.global.annotation.architecture.PersistenceAdapter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static codeping.flex.investment.global.config.openai.OpenAiPrompts.INVESTMENT_HISTORY_FORMAT;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class InvestmentAnalysisPersistenceAdapter implements InvestmentAnalysisOutPort {
+
+    private final InvestmentRepository investmentRepository;
+    private final InvestmentPersistenceMapper investmentPersistenceMapper;
+
+    @Override
+    public String getAllInvestmentsByUserId(Long userId) {
+        final List<InvestmentEntity> investmentEntities = this.investmentRepository.findAllByUserId(userId);
+        return investmentEntities.stream()
+                .map(investment -> {
+                    Investment domain = this.investmentPersistenceMapper.toDomain(investment);
+                    return String.format(INVESTMENT_HISTORY_FORMAT,
+                            domain.getStockCode(),
+                            domain.getCorpName(),
+                            domain.getInvestType().name(),
+                            domain.getQuantity(),
+                            domain.getPrice().toString(),
+                            domain.getTotalPrice().toString(),
+                            domain.getProfit().toString(),
+                            domain.getCreatedAt().toString());
+                })
+                .collect(Collectors.joining(",", "[", "]"));
+    }
+
+}
--- src/main/java/codeping/flex/investment/adapter/out/persistence/repository/InvestmentRepository.java
+++ src/main/java/codeping/flex/investment/adapter/out/persistence/repository/InvestmentRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InvestmentRepository extends JpaRepository<InvestmentEntity, Long> {
 
     Slice<InvestmentEntity> findAllByUserIdAndStockCode(Long userId, String stockCode, Pageable pageable);
+    List<InvestmentEntity> findAllByUserId(Long userId);
 }
